43f2c080e8032a0571f5eb068e19998024fef341
haven <haven@actio.com.au>
2017-07-08 09:36:31 +1000

    country codes

--- src/components/quick-quote/QuickQuotePage.js
+++ src/components/quick-quote/QuickQuotePage.js
@@ -5,55 +5,153 @@ import * as peopleActions from '../../actions/peopleActions';
 import * as ratingsActions from '../../actions/ratingsActions';
 import {store} from '../../index';
 import * as PropTypes from 'react/lib/ReactPropTypes';
-import {Button, Checkbox, Col, Form, FormControl, FormGroup} from 'react-bootstrap';
+import {
+  Button, ControlLabel, DropdownButton, Form, FormControl, FormGroup, InputGroup,
+  MenuItem
+} from 'react-bootstrap';
+import {countryCodes} from "../../common/countryCodes";
 
 
 
 class QuickQuotePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.getValidationStateRequired = this.getValidationStateRequired.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleCountryCodeSelect = this.handleCountryCodeSelect.bind(this);
+
+    this.countryCodes = countryCodes;
+
+    this.state = {
+      firstName: '',
+      firstNameTouched: null,
+      lastName: '',
+      lastNameTouched: null,
+      email: '',
+      countryCode:countryCodes[0].dial_code
+    };
   }
 
   componentDidMount() {
     store.dispatch(peopleActions.loadPeople());
   }
 
+  getValidationStateRequired(input) {
+    const inputTouched = `${input}Touched`;
+    if(this.state[inputTouched]) {
+      const length = this.state[input].length;
+      return length > 0 ? 'success':'error';
+    }
+    else {
+      return null;
+    }
+  }
+
+  getValidationStateEmail(input) {
+    const inputTouched = `${input}Touched`;
+    if(this.state[inputTouched]) {
+      const value = this.state[input];
+      const emailPattern = new RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$");
+      return emailPattern.test(value) ? 'success':'error';
+    }
+    else {
+      return null;
+    }
+  }
+
+  handleChange(e) {
+    const id = e.target.id;
+    this.setState(Object.assign({}, this.state, { [id]: e.target.value }));
+  }
+
+  handleCountryCodeSelect(e) {
+    this.setState(Object.assign({}, this.state, { countryCode: e.target.text }));
+  }
+
+  handleFocus(e) {
+    const idTouched = `${e.target.id}Touched`;
+    if(!this.state[idTouched]) {
+      this.setState(Object.assign({}, this.state, { [idTouched]: true }));
+    }
+  }
+
   render() {
-    const ControlLabel = 'quick-quote';
+    const countryCodes = this.countryCodes.map((countryCode) => {
+      return <MenuItem key={`${countryCode.name}`} onClick={this.handleCountryCodeSelect}>{countryCode.dial_code}</MenuItem>;
+    });
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-            Email
-          </Col>
-          <Col sm={10}>
-            <FormControl type="email" placeholder="Email" />
-          </Col>
-        </FormGroup>
+      <div className="col-sm-12">
+        <Form>
+          <div className="col-sm-6">
+            <FormGroup
+              controlId="firstName"
+              validationState={this.getValidationStateRequired('firstName')}>
+              <ControlLabel className="required">First Name</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.firstName}
+                placeholder="First Name"
+                onFocus={this.handleFocus}
+                onChange={this.handleChange}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </div>
 
-        <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={2}>
-            Password
-          </Col>
-          <Col sm={10}>
-            <FormControl type="password" placeholder="Password" />
-          </Col>
-        </FormGroup>
+          <div className="col-sm-6">
+            <FormGroup
+              controlId="lastName"
+              validationState={this.getValidationStateRequired('lastName')}>
+              <ControlLabel className="required">Last Name</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.lastName}
+                placeholder="Last Name"
+                onFocus={this.handleFocus}
+                onChange={this.handleChange}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </div>
 
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Checkbox>Remember me</Checkbox>
-          </Col>
-        </FormGroup>
+          <div className="col-sm-12">
+            <FormGroup
+              controlId="email"
+              validationState={this.getValidationStateEmail('email')}>
+              <ControlLabel>Email</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.email}
+                placeholder="Email"
+                onFocus={this.handleFocus}
+                onChange={this.handleChange}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </div>
 
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Button type="submit">
-              Sign in
-            </Button>
-          </Col>
-        </FormGroup>
-      </Form>
+          <div className="col-sm-12">
+            <FormGroup>
+              <ControlLabel>Telephone / Mobile</ControlLabel>
+              <InputGroup>
+                <DropdownButton
+                  componentClass={InputGroup.Button}
+                  id="input-dropdown-addon"
+                  title={this.state.countryCode}>
+                  {countryCodes}
+                </DropdownButton>
+                <FormControl type="text" />
+              </InputGroup>
+            </FormGroup>
+          </div>
+
+          <Button type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
