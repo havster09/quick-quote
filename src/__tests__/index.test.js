f3dcdbac97f28493a2db8015da2168c763e0a336
haven <haven@actio.com.au>
2017-09-02 11:15:27 +1000

    shallow redux form testing

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount, shallow, render } from 'enzyme';
 import QuickQuoteRefTest from '../components/quick-quote/QuickQuoteRefTest';
-
+import SimpleWrapperConnected, { SimpleWrapper } from '../components/redux_forms/SimpleWrapper';
 
 const smFunc = jest.fn();
+
 
 beforeEach(() => {
   smFunc(1);
 });
 
-const wrapper = shallow(<QuickQuoteRefTest/>);
+let wrapper = shallow(<QuickQuoteRefTest />);
 
 describe('Our first test', () => {
   const n = null;
@@ -24,5 +25,15 @@ describe('Our first test', () => {
   it('should pass again', () => {
     expect(smFunc).toHaveBeenCalledWith(1);
     expect(smFunc).toHaveBeenCalled();
+  });
+});
+
+describe('redux shallow', () => {
+  wrapper = shallow(<SimpleWrapper />);
+  const ComponentInstance = wrapper.instance();
+  const spy = jest.spyOn(ComponentInstance, 'smFunc2');
+  it('should invoke smFunc2', () => {
+    ComponentInstance.handleSuccess();
+    expect(spy).toHaveBeenCalledWith('yay');
   });
 });
--- src/components/redux_forms/SimpleWrapper.js
+++ src/components/redux_forms/SimpleWrapper.js
@@ -4,27 +4,50 @@ import { connect } from 'react-redux';
 import { submit } from 'redux-form';
 import submitAction from './submit';
 
-const SimpleWrapper = ({ invokeSubmit }) => {
+export class SimpleWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSuccess = this.handleSuccess.bind(this);
+    this.smFunc2 = this.smFunc2.bind(this);
+  }
+
+  smFunc2(msg) {
+    console.log('handleSuccess', msg);
+  }
+
+  handleSuccess() {
+    this.smFunc2('yay');
+    return 'handleSuccess';
+  }
+
+  render() {
   return (
     <div>
-      <button onClick={invokeSubmit}>Invoke</button>
+        <button onClick={this.props.invokeSubmit}>Invoke</button>
       <SimpleForm
-        onSubmitSuccess={() => console.log('success callback')}
+          onSubmitSuccess={this.handleSuccess}
         onSubmit={submitAction} />
     </div>
   );
-};
+  }
+
+}
 
 SimpleWrapper.propTypes = {};
 
 const mapStateToProps = (state, ownProps) => {
-  return {};
+  return {
+    crap: 'crap'
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     invokeSubmit: () => {
       dispatch(submit('simple'));
+    },
+    handleSuccess: () => {
+      console.log('handleSuccess');
     }
   };
 };
